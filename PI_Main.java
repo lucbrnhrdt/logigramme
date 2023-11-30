db3d296c95cea8e64f8eff7780ef06a47ca342f6
Luc Bernhardt <123077431+lucbrnhrdt@users.noreply.github.com>
2023-11-30 10:54:06 +0100

    V1.3

--- PI_Main.java
+++ PI_Main.java
@@ -6,24 +6,12 @@ public class PI_Main {
 	System.out.println("\nVeuillez vérifier que le fichier algorithme est dans le même dossier que ce programme Java.");
 	System.out.println("\nEntrez le nom de votre fichier algorithme (en .txt) à convertir :");
 	nom_fichier=Lire.S();
-	String FichierAlgo=nom_fichier; //variable String du nom du fichier txt de l'algorithme
-	String [] Algo = new String[100]; //on créé un tableau de 100 chaînes de caractère de façon à manipuler les lignes de l'algorithme plus facilement
+	
 	File file = new File("CodeTikz.txt"); //création d'un fichier txt en sortie (pour le code Tikz)
 	file.delete();
 	
-	/* Partie 1 : conversion du fichier txt en tableau de chaînes de caractères*/
-	try { //try / catch : gestion des exceptions qui peuvent survenir (ici, le fait que le fichier n'existe pas dans le dossier parent de main.java)
-    BufferedReader reader = new BufferedReader(new FileReader(FichierAlgo)); //assignation du fichier texte d'entré à utilisé pour l'algo, en utilisant la classe BufferedReader (issu de java.io)
-    String line;
-    int i=0;
-    while ((line = reader.readLine()) != null) {
-		Algo[i]=line; //on assigne la chaîne de caractère de la ligne i dans le tableau Algo au rang i.
-		i=i+1;
-		}
-    reader.close();
-    } catch (IOException e) { //si l'exception est vérifiée, alors on renvoie l'erreur à l'utilisateur
-		System.out.println("Erreur");
-	}
+	String[] Algo; 
+    Algo = Conversion.convertir(nom_fichier); //fonction qui convertir un fichier txt en tableau
 	
 	/* Partie 2 : conversion des chaines de caractères en code Tikz*/
 	try {
@@ -43,6 +31,7 @@ public class PI_Main {
         writer.flush();
 		int i=0;
 		int j=1;
+		int temp=0;
 		if (Algo[i]!="Début"){
 			i=i+1;
 			}
@@ -71,14 +60,12 @@ public class PI_Main {
 				Algo[i]=Algo[i].replace(">=","$>$=");
 				Algo[i]=Algo[i].replace("!=","$!$=");
 				writer.append("\n\\node[losange] (t"+j+") [below =of t"+(j-1)+"] {"+Algo[i].replace(" si","")+"};");
+				temp=j;
 				writer.flush();
 				i=i+1;
 				j=j+1;
 			} else if (Algo[i].startsWith("sinon")){ //gestion de la structure conditionnelle "sinon"
-				Algo[i]=Algo[i].replace("<=","$<$=");
-				Algo[i]=Algo[i].replace(">=","$>$=");
-				Algo[i]=Algo[i].replace("!=","$!$=");
-				writer.append("\n\\node[losange] (t"+j+") [below =of t"+(j-1)+"] {"+Algo[i].replace(" sinon","")+"};");
+				writer.append("\n\\node[losange] (t"+temp+") [right =of t"+(temp)+"] {"+Algo[i].replace(" sinon","")+"};");
 				writer.flush();
 				i=i+1;
 				j=j+1;
